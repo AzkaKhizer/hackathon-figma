978dc69eef7211eb642f00a5bf6b7f1bfd62785a
AzkaKhizer <144557215+AzkaKhizer@users.noreply.github.com>
2025-01-10 18:30:33 +0500

    fonts.tsx

--- src/components/fonts.tsx
+++ src/components/fonts.tsx
@@ -1,7 +1,10 @@
-import {Playfair_Display,Cinzel,Prata,Montserrat} from "next/font/google";
+import {Playfair_Display,Inter,Cinzel,Prata,Montserrat} from "next/font/google";
 const Playfair = Playfair_Display({subsets:["latin"]});
 const Cinze = Cinzel({subsets:["latin"]});
-const Cinzel = Cinzel({subsets:["latin"]});
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 const Prat = Prata({subsets:["latin"],weight:"400"});
 const Montserratt = Montserrat({subsets:["latin"]});
 
@@ -10,7 +13,7 @@ export default function Fonts(){
         <div className="bg-black w-full h-[122px] flex justify-center space-x-3 md:justify-evenly items-center flex-wrap max-w-screen-2xl  mx-auto">
             <h1 className={`${Playfair.className} text-white text-2xl md:text-4xl` }>VERSACE</h1>
             <h1 className={`${Cinze.className} text-white text-2xl md:text-4xl`}>ZARA</h1>
-            <h1 className={`${Cinzel.className} text-white text-2xl md:text-4xl`}>GUCCI</h1>
+            <h1 className={`${inter.className} text-white text-2xl md:text-4xl`}>GUCCI</h1>
             <h1 className={`${Prat.className} text-white text-2xl md:text-4xl`}>PRADA</h1>
             <h1 className={`${Montserratt.className} text-white text-2xl md:text-4xl`}>Calvin Klein</h1>
             
