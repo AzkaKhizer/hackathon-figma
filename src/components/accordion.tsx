84e3bd6a6bed058e3a53815397e71deacde39f69
AzkaKhizer <144557215+AzkaKhizer@users.noreply.github.com>
2025-01-10 17:35:18 +0500

    accordion.tsx

--- src/components/accordion.tsx
+++ src/components/accordion.tsx
@@ -27,20 +27,20 @@ import { FiSliders } from "react-icons/fi"
           <AccordionTrigger>Is it styled?</AccordionTrigger>
           <AccordionContent>
             Yes. It comes with default styles that matches the other
-            components&apos; aesthetic.
+            components.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-3">
           <AccordionTrigger>Is it animated?</AccordionTrigger>
           <AccordionContent>
-            Yes. It's animated by default, but you can disable it if you prefer.
+            Yes. It is animated by default, but you can disable it if you prefer.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-4">
           <AccordionTrigger>Is it styled?</AccordionTrigger>
           <AccordionContent>
             Yes. It comes with default styles that matches the other
-            components&apos; aesthetic.
+            components.
           </AccordionContent>
         </AccordionItem>
       </Accordion>
