dbddb66901d81f37c9c070f1bfe82525acce8107
AzkaKhizer <144557215+AzkaKhizer@users.noreply.github.com>
2025-01-10 17:03:14 +0500

    Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,25 +1,52 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 
 export default function Hero() {
   return (
-    <main className='w-full h-[615px] flex justify-between items-start bg-[#F2F0F1]'>
-    <div className='relative'>
-
-      <div className='absolute w-[315px]  md:w-[577px] mt-3 md:mt-10 ml-5 md:ml-10'>
-          <h1 className='font-bold text-[36px] md:text-[64px]'>FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
-          <p className='text-[14px] md:text-[16px] text-[#655B5B] mt-3'>Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
-        <button className='text-white bg-[#000000] py-2 px-8 rounded-[62px] text-[16px] mt-4  '>Shop Now</button>
-      </div>
-
-      <div className='absolute'>
-          <Image src={"/Rectangle2.png"} className=' mr-4' width={1400} height={615} alt="profile"></Image>
-          <Image src={"/Vector-3.png"} className=' mr-4 ' width={100} height={100} alt="star"></Image>
-      </div>
+    <div className='bg-[#F2F0F1] flex flex-col max-w-screen-2xl  mx-auto  w-full h-full md:mt-2 '>
       
+    <div className='md:absolute flex flex-col p-4  md:pl-10 h-full justify-start pt-4 items-start w-full  md:max-w-[50%] '>
+      <h1 className='text-black md:text-[64px] font-bold md:font-extrabold text-[36px]'>FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
+      <p className='text-[#655B5B] md:text-[16px]  pt-4 text-[14px]'>Browse Through Our Diverse Range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
+      <button className='bg-black text-white w-full rounded-full px-3 py-3 mt-6  md:w-[210px] md:h-[52px] transform transition-transform duration-300 hover:scale-110 hover:shadow-lg '>Shop Now</button>
+
+
+      <div className='w-[278px] md:w-[596px] h-full flex flex-wrap space-x-2 mt-4  pl-8 md:pl-0  ml-4 md:ml-0  md:mr-4 justify-center md:justify-start items-center '>
+        <div className='flex-1 text-center md:text-left h-full mr-20 md:mr-0'>
+          <h2 className='font-bold text-[24px] md:text-[40px]'>200+</h2>
+          <p className='text-[12px] md:text-[16px] text-[#655B5B]'>International Brands</p>
+        </div>
+          
+        <div className='flex-1 text-center md:text-left h-full'>
+          <h2 className='font-bold text-[24px] md:text-[40px]'>2000+</h2>
+          <p className='text-[12px] md:text-[16px] text-[#655B5B]'>High Quality Products</p> 
+        </div>
+          
+        
+        <div className='flex-1 text-center md:text-left h-full'>
+          <h2 className='font-bold text-[24px] md:text-[40px]'>3000+</h2>
+          <p className='text-[12px] md:text-[16px] text-[#655B5B]'>Happy Customers</p>
+          
+        </div>
+
+        </div>
+       
     </div>
-    </main>
-  )
-}
+<div>
+    <div className='h-screen md:h-full   bg-[#F2F0F1] w-full hidden md:block '>
+   
+   <Image src="/Rectangle.png" className=' w-full h-full object-cover' width={1440} height={700} alt="Clothing" />
+     </div>
+     <div className='block md:hidden h-screen bg-[#F2F0F1] w-full'>
+      <Image src="/Rectangle2.png" className=' w-full h-full object-cover' alt="clothing" width={390} height={448}  />
+      </div>
 
-
+     <div className='max-w-screen-2xl  mx-auto'>
+      <Image src="/Vector-3.png" className=' absolute top-[700px] right-[1px] md:top-[150px] md:right-[50px] w-[66px] h-[66px] md:w-[104px] md:h-[104px] ' alt="star" width={104} height={104}/>
+     </div>
+     <div>
+      <Image src="/Vector-4.png" className='absolute top-[850px]  right-[335px] md:top-[330px] md:left-[700px] w-[34px] h-[34px] md:w-[56px] md:h-[56px] ' alt="star" width={56} height={56}/>
+     </div>
+     </div>
+     </div>
+     )}
